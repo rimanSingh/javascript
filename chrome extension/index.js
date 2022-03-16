0fdc15d9f463fc7cf5ed0fcf0e848f8bf7fe7293
rimanSingh <rimandeep267@gmail.com>
2022-03-16 20:45:17 +0100

    extension commit

---
+++ chrome extension/index.js
@@ -0,0 +1,19 @@
+
+let myLinks = []
+const userInput = document.getElementById("extension")
+let btnClick = document.getElementById("extension-btn")
+const ulList = document.getElementById("ul-list")
+
+btnClick.addEventListener("click", function () {
+    myLinks.push(userInput.value)
+    userInput.value=""
+    render()
+})
+
+function render() {
+    let listItems=""
+    for (let i = 0; i < myLinks.length; i++) {
+        listItems += "<li> <a target='_blank' href='" + myLinks[i] +"'>" + myLinks[i] + "</a></li>"
+    }
+    ulList.innerHTML=listItems
+}
