7c2bb0c619ae57a1b50eca503b33516f8880d8cb
rimanSingh <rimandeep267@gmail.com>
2022-03-16 18:01:32 +0100

    Add_Player Commit

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,13 +1,11 @@
-let randomNumber = Math.random()
-
-let firstcard = getRandomCard()
-let secondcard = getRandomCard()
-
-let cards = [firstcard, secondcard]
-let sumOfCards = firstcard + secondcard
-
-let hasBlackJack = false
-let isAlive = true
+let player = {
+    name : "ABC",
+    chips : 150
+}
+let cards = []
+let sumOfCards = 0
+let blackJack = false
+let isAlive = false
 let message = ""
 
 let messageEl = document.getElementById("message-pl")
@@ -15,6 +13,8 @@ let messageEl = document.getElementById("message-pl")
 let sumEl = document.querySelector("#sum-el")
 let cardEl = document.getElementById("card-el")
 
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
@@ -29,8 +29,13 @@ function getRandomCard() {
     }
 }
 
-
 function startGame() {
+    isAlive = true
+    let firstcard = getRandomCard()
+    let secondcard = getRandomCard()
+
+    cards = [firstcard, secondcard]
+    sumOfCards = firstcard + secondcard
     game()
 }
 
@@ -39,14 +44,13 @@ function game() {
     for (let i = 0; i < cards.length; i++) {
         cardEl.textContent += cards[i] + " "
     }
-
     sumEl.textContent = "Sum: " + sumOfCards
     if (sumOfCards <= 20) {
         message = "Do you want to add a new card?"
     }
     else if (sumOfCards === 21) {
         message = "You've got Blackjack!"
-        blackjack = true
+        blackJack = true
     }
     else {
         message = "You're out of the game"
@@ -56,12 +60,11 @@ function game() {
 }
 
 function newCard() {
-    console.log("New Card")
-
-    let newCard = getRandomCard()
-    sumOfCards += newCard
-    cards.push(newCard)
-    console.log(cards)
-    game()
-
+    if(isAlive ===true && blackJack===false) {
+        let newCard = getRandomCard()
+        sumOfCards += newCard
+        cards.push(newCard)
+        console.log(cards)
+        game()
+    }
 }
